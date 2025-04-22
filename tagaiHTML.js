b9b088d58244ceda5421752dcc1247d0e12e0caf
Kornelija Večkienė <kornelija.acisauskaite@gmail.com>
2025-04-22 13:05:46 +0300

    html tagai

---
+++ tagaiHTML.js
@@ -0,0 +1,124 @@
+//*
+//Žyma | Aprašymas
+//<!--...--> | Apibrėžia komentarą
+//<!DOCTYPE> | Nurodo dokumento tipą
+//<a> | Apibrėžia hipersaitą (nuorodą)
+//<abbr> | Apibrėžia trumpinį ar akronimą
+//<acronym> | Nenaudojama HTML5. Vietoj to naudokite <abbr>. Apibrėžia akronimą
+//<address> | Nurodo dokumento autoriaus/savininko kontaktinę informaciją
+//<applet> | Nenaudojama HTML5. Naudokite <embed> arba <object> vietoj to
+//<area> | Apibrėžia sritį paveikslėlio žemėlapyje
+//<article> | Apibrėžia straipsnį
+//<aside> | Turinys, esantis šalia pagrindinio puslapio turinio
+//<audio> | Įterptas garso turinys
+//<b> | Paryškintas tekstas
+//<base> | Nurodo bazinį URL visoms santykinėms nuorodoms dokumente
+//<basefont> | Nenaudojama HTML5. Naudokite CSS vietoj to
+//<bdi> | Izoliuoja teksto dalį, kuri gali būti parašyta kita kryptimi
+//<bdo> | Pakeičia esamą teksto kryptį
+//<big> | Nenaudojama HTML5. Naudokite CSS vietoj to
+//<blockquote> | Citata iš kito šaltinio
+//<body> | Dokumento kūnas
+//<br> | Naujos eilutės ženklas
+//<button> | Mygtukas
+//<canvas> | Leidžia piešti grafiką naudojant JavaScript
+//<caption> | Lentelės antraštė
+//<center> | Nenaudojama HTML5. Naudokite CSS vietoj to
+//<cite> | Kūrinio pavadinimas
+//<code> | Kompiuterio kodo fragmentas
+//<col> | Lentelės stulpelio savybės
+//<colgroup> | Stulpelių grupė lentelėje
+//<data> | Prideda mašinai suprantamą vertę turiniui
+//<datalist> | Iš anksto apibrėžtų pasirinkimų sąrašas
+//<dd> | Termino apibrėžimas aprašų sąraše
+//<del> | Pašalintas tekstas
+//<details> | Papildoma informacija, kurią galima išskleisti
+//<dfn> | Terminas, kuris bus apibrėžtas
+//<dialog> | Dialogo langas
+//<dir> | Nenaudojama HTML5. Naudokite <ul> vietoj to
+//<div> | Dokumento skyrius
+//<dl> | Aprašų sąrašas
+//<dt> | Terminas aprašų sąraše
+//<em> | Pabrėžtas tekstas (emfazuotas)
+//<embed> | Išorinių programų konteineris
+//<fieldset> | Susijusių formos laukų grupė
+//<figcaption> | Antraštė prie <figure> elemento
+//<figure> | Savarankiškas turinys
+//<font> | Nenaudojama HTML5. Naudokite CSS vietoj to
+//<footer> | Puslapio ar skyriaus poraštė
+//<form> | HTML forma naudotojo įvedimui
+//<frame> | Nenaudojama HTML5
+//<frameset> | Nenaudojama HTML5
+//<h1> iki <h6> | Antraštės nuo svarbiausios (h1) iki mažiausios (h6)
+//<head> | Dokumento metainformacija
+//<header> | Dokumento ar skyriaus antraštė
+//<hgroup> | Grupė antraščių
+//<hr> | Teminė pertrauka (horizontali linija)
+//<html> | HTML dokumento šaknis
+//<i> | Tekstas kitokiu tonu ar stiliumi
+//<iframe> | Įterptas kitas puslapis (rėmelis)
+//<img> | Vaizdas (paveikslėlis)
+//<input> | Įvedimo laukas
+//<ins> | Įterptas tekstas
+//<kbd> | Klaviatūros įvestis
+//<label> | Žyma įvedimo laukui
+//<legend> | Pavadinimas <fieldset>
+//<li> | Sąrašo elementas
+//<link> | Nuoroda į išorinį šaltinį (dažniausiai stilių lentelę)
+//<main> | Pagrindinis dokumento turinys
+//<map> | Paveikslėlio žemėlapis
+//<mark> | Pažymėtas/akcentuotas tekstas
+//<menu> | Nesurikiuotas sąrašas
+//<meta> | Metainformacija apie dokumentą
+//<meter> | Matavimo rodmuo (pvz., slėgio, progreso)
+//<nav> | Naršymo nuorodos
+//<noframes> | Nenaudojama HTML5
+//<noscript> | Turinys vartotojams be JavaScript
+//<object> | Išorinės programos konteineris
+//<ol> | Surikiuotas sąrašas
+//<optgroup> | Susijusių pasirinkimų grupė sąraše
+//<option> | Vienas pasirinkimas sąraše
+//<output> | Skaičiavimo rezultatas
+//<p> | Pastraipa
+//<param> | Parametras objektui
+//<picture> | Konteineris keliems paveikslėliams
+//<pre> | Iš anksto suformatuotas tekstas
+//<progress> | Užduoties progreso juosta
+//<q> | Trumpa citata
+//<rp> | Tekstas naršyklėms, kurios nepalaiko ruby anotacijų
+//<rt> | Paaiškinimas/pronunciacija (rytų Azijos tipografijai)
+//<ruby> | Ruby anotacija
+//<s> | Netinkamas/atšauktas tekstas
+//<samp> | Pavyzdinis kompiuterio programos išvesties tekstas
+//<script> | Kliento pusės scenarijus
+//<search> | Paieškos sritis
+//<section> | Dokumento skyrius
+//<select> | Iškrentantis pasirinkimų sąrašas
+//<small> | Mažesnis tekstas
+//<source> | Kelios laikmenos (video, audio) parinktys
+//<span> | Teksto fragmentas
+//<strike> | Nenaudojama HTML5. Naudokite <del> arba <s>
+//<strong> | Svarbus tekstas
+//<style> | Stiliaus informacija dokumentui
+//<sub> | Indeksas apačioje
+//<summary> | Matoma antraštė <details> elemente
+//<sup> | Indeksas viršuje
+//<svg> | SVG grafikos konteineris
+//<table> | Lentelė
+//<tbody> | Lentelės turinio grupė
+//<td> | Lentelės langelis
+//<template> | Šablonas, nematomas įkrovus puslapį
+//<textarea> | Kelių eilučių teksto įvedimo laukas
+//<tfoot> | Lentelės poraštės turinys
+//<th> | Lentelės antraštės langelis
+//<thead> | Lentelės antraštės grupė
+//<time> | Laiko/datos reikšmė
+//<title> | Dokumento pavadinimas
+//<tr> | Lentelės eilutė
+//<track> | Subtitrai ar kita medijos informacija
+//<tt> | Nenaudojama HTML5. Naudokite CSS
+//<u> | Pabrauktas tekstas
+//<ul> | Nesurikiuotas sąrašas
+//<var> | Kintamasis
+//<video> | Vaizdo turinys
+//<wbr> | Galimas eilutės pertraukos taškas
